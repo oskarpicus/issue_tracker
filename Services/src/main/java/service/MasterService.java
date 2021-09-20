58fb051b14cc1cbd6af322366b31fe53e8d4fe17
Oskar Picus <oskarpicus@yahoo.com>
2021-09-20 14:43:08 +0300

    Implemented create account in service

---
+++ Services/src/main/java/service/MasterService.java
@@ -0,0 +1,31 @@
+package service;
+
+import exceptions.EmailTakenException;
+import exceptions.UsernameTakenException;
+import model.User;
+import repository.UserRepository;
+import utils.Constants;
+
+import java.util.Optional;
+
+public class MasterService implements Service {
+    private final UserRepository userRepository;
+
+    public MasterService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createAccount(User user) throws UsernameTakenException, EmailTakenException {
+        if (userRepository.findUserByUsername(user.getUsername()).isPresent()) {
+            throw new UsernameTakenException(Constants.USERNAME_TAKEN_ERROR_MESSAGE);
+        }
+
+        if (userRepository.findUserByEmail(user.getEmail()).isPresent()) {
+            throw new EmailTakenException(Constants.EMAIL_TAKEN_ERROR_MESSAGE);
+        }
+
+        Optional<User> result = userRepository.save(user);
+        return result.isEmpty() ? user : null;
+    }
+}
---
+++ Services/src/main/java/utils/Constants.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Constants {
+
+    /**
+     * Error message displayed when creating a new account, if the respective username is already in use
+     */
+    public static final String USERNAME_TAKEN_ERROR_MESSAGE = "This username is already registered";
+
+    /**
+     * Error message displayed when creating a new account, if the respective email is already in use
+     */
+    public static final String EMAIL_TAKEN_ERROR_MESSAGE = "This email is already used";
+}
