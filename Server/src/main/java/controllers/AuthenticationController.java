1f2ebf54ff2a46d8c36df02f9a2554f9385c9e7a
Oskar Picus <oskarpicus@yahoo.com>
2021-10-29 11:22:40 +0300

    Updated front end to use BE authentication

--- Server/src/main/java/controllers/AuthenticationController.java
+++ Server/src/main/java/controllers/AuthenticationController.java
@@ -82,7 +82,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers(UriMapping.CREATE_ACCOUNT, UriMapping.LOGGING)
                 .permitAll()
