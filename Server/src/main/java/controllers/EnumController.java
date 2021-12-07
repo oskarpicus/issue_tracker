bbb611290f3e200562f36fc663fccfd0a1bc61f0
Oskar Picus <oskarpicus@yahoo.com>
2021-12-07 19:28:30 +0200

    Moved get roles controller

--- Server/src/main/java/controllers/EnumController.java
+++ Server/src/main/java/controllers/EnumController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import model.IssueType;
+import model.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,5 +14,10 @@ public class EnumController {
     @GetMapping(value = "/issue-types")
     public ResponseEntity<IssueType[]> getIssueTypes() {
         return new ResponseEntity<>(IssueType.values(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/roles")
+    public ResponseEntity<Role[]> getAllRoles() {
+        return new ResponseEntity<>(Role.values(), HttpStatus.OK);
     }
 }
