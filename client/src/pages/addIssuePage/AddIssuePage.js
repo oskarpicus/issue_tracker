1e5dfbb66c9bc3651bc98efe9c8ea526c6a395a7
Oskar Picus <oskarpicus@yahoo.com>
2021-12-07 21:36:03 +0200

    Implemented design for Add Issue Page

--- client/src/pages/addIssuePage/AddIssuePage.js
+++ client/src/pages/addIssuePage/AddIssuePage.js
@@ -1,11 +1,169 @@
-import {withRouter} from "react-router-dom";
+import {Redirect, useHistory, withRouter} from "react-router-dom";
 import './AddIssuePage.css';
+import {errorPage, responseTypes} from "../../components/const";
+import {useEffect, useState} from "react";
+import {Box, Button} from "@mui/material";
+import Menu from "../../components/menu/Menu";
+import MyComboBox from "../../components/myComboBox/MyComboBox";
+import LabeledField from "../../components/labeledField/LabeledField";
+import {getAllIssueTypes, getAllSeverities} from "../../services/enumService";
+import {formatEnum} from "../../components/utils";
+
+const getProjectParticipants = (project) => {
+    if (project === null || project === undefined) {
+        return [];
+    }
+
+    return project.involvements.map(involvement => involvement.user)
+}
 
 const AddIssue = (properties) => {
-    console.log(properties.credentials);
-    return (
-        <div>Ana are mere</div>
-    )
+    const credentials = properties.credentials === undefined ?
+        properties.location.state.credentials : properties.credentials;
+
+    const [formValues, setFormValues] = useState({
+        project: properties.location.state ? properties.location.state.project : undefined,
+        title: "",
+        description: "",
+        severity: "",
+        type: "",
+        expectedBehaviour: "",
+        actualBehaviour: "",
+        stackTrace: "",
+        assignee: undefined
+    });
+
+    console.log(formValues);
+
+    const [severities, setSeverities] = useState([]);
+
+    const [issueTypes, setIssueTypes] = useState([]);
+
+    let history = useHistory();
+
+    useEffect(() => {
+        if (credentials.jwt !== "") {
+            getAllIssueTypes(credentials.jwt)
+                .then(response => {
+                    if (response[responseTypes.key] === responseTypes.success) {
+                        setIssueTypes(response.data);
+                    } else {
+                        history.push(errorPage);
+                    }
+                });
+            getAllSeverities(credentials.jwt)
+                .then(response => {
+                    if (response[responseTypes.key] === responseTypes.success) {
+                        setSeverities(response.data);
+                    } else {
+                        history.push(errorPage);
+                    }
+                })
+        }
+    }, [credentials, history]);
+
+    if (credentials.jwt === "") {
+        return <Redirect to={errorPage}/>;
+    }
+
+    const handleClick = () => {
+        // todo send request to save issue
+        // prepare to use ids
+    };
+
+    const content = (
+        <Box>
+            <p className={"action-title"}>Add a new issue</p>
+            <MyComboBox
+                className={"combo-box-filled-label"}
+                getOptionLabel={option => option.title}
+                label={"Project"}
+                defaultValue={properties.location.state ? properties.location.state.project : undefined}
+                options={[properties.location.state.project]}  // todo fill the options with the user's projects
+                onChange={(event, value) => setFormValues((prev) => ({...prev, project: value}))}
+            />
+            <LabeledField
+                text={"Title"}
+                name={"title"}
+                type={"input"}
+                onChange={(name, value) => setFormValues((prev) => ({...prev, [name]: value}))}
+            />
+            <LabeledField
+                text={"Description"}
+                name={"description"}
+                type={"input"}
+                onChange={(name, value) => setFormValues((prev) => ({...prev, [name]: value}))}
+            />
+            <MyComboBox
+                getOptionLabel={option => formatEnum(option)}
+                label={"Severity"}
+                className={"combo-box-filled-label combo-box-add-issue-enum"}
+                options={severities}
+                onChange={(event, value) => setFormValues((prev) => ({...prev, severity: value}))}
+            />
+            <MyComboBox
+                getOptionLabel={option => formatEnum(option)}
+                label={"Type"}
+                className={"combo-box-filled-label combo-box-add-issue-enum"}
+                options={issueTypes}
+                onChange={(event, value) =>
+                    setFormValues((prev) => ({
+                        ...prev,
+                        type: value,
+                        expectedBehaviour: value !== "BUG" && "",
+                        actualBehaviour: value !== "BUG" && "",
+                        stackTrace: value !== "BUG" && ""
+                    }))
+                }
+            />
+            {
+                formValues.type === "BUG"
+                &&
+                <LabeledField
+                    text={"Expected Behaviour"}
+                    name={"expectedBehaviour"}
+                    type={"input"}
+                    onChange={(name, value) => setFormValues((prev) => ({...prev, [name]: value}))}
+                />
+            }
+            {
+                formValues.type === "BUG"
+                &&
+                <LabeledField
+                    text={"Actual Behaviour"}
+                    name={"actualBehaviour"}
+                    type={"input"}
+                    onChange={(name, value) => setFormValues((prev) => ({...prev, [name]: value}))}
+                />
+            }
+            {
+                formValues.type === "BUG"
+                &&
+                <LabeledField
+                    text={"Stack Trace"}
+                    name={"stackTrace"}
+                    type={"input"}
+                    onChange={(name, value) => setFormValues((prev) => ({...prev, [name]: value}))}
+                />
+            }
+            <MyComboBox
+                getOptionLabel={option => option.username}
+                label={"Assignee"}
+                className={"combo-box-filled-label combo-box-add-issue-enum"}
+                options={getProjectParticipants(formValues.project)}
+                onChange={(event, value) => setFormValues((prev) => ({...prev, assignee: value}))}
+            />
+            <Button
+                variant={"contained"}
+                className={"action-button"}
+                onClick={handleClick}
+            >
+                Add issue
+            </Button>
+        </Box>
+    );
+
+    return <Menu content={content} credentials={credentials}/>;
 };
 
 const AddIssuePage = withRouter(AddIssue);
