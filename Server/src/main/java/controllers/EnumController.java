0d0c3fbaa30c494b52664d0c57f1f5e8cf839720
Oskar Picus <oskarpicus@yahoo.com>
2021-12-07 19:36:51 +0200

    Implemented the rest of the enum controllers

--- Server/src/main/java/controllers/EnumController.java
+++ Server/src/main/java/controllers/EnumController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import model.IssueType;
 import model.Role;
+import model.Severity;
+import model.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,7 +19,17 @@ public class EnumController {
     }
 
     @GetMapping(value = "/roles")
-    public ResponseEntity<Role[]> getAllRoles() {
+    public ResponseEntity<Role[]> getRoles() {
         return new ResponseEntity<>(Role.values(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/severities")
+    public ResponseEntity<Severity[]> getSeverities() {
+        return new ResponseEntity<>(Severity.values(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/statuses")
+    public ResponseEntity<Status[]> getStatuses() {
+        return new ResponseEntity<>(Status.values(), HttpStatus.OK);
     }
 }
