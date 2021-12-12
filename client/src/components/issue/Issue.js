28ab8229e27f0ebea32e7145c17000588b38509d
Oskar Picus <oskarpicus@yahoo.com>
2021-12-12 16:42:50 +0200

    Moved icon for issue computation in utils

--- client/src/components/issue/Issue.js
+++ client/src/components/issue/Issue.js
@@ -1,43 +1,16 @@
 import {Box, Tooltip} from "@mui/material";
 import './issue.css';
-import BugReportIcon from "@mui/icons-material/BugReportOutlined";
-import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
-import DescriptionIcon from '@mui/icons-material/Description';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import HelpIcon from '@mui/icons-material/Help';
-import ClearIcon from '@mui/icons-material/Clear';
-import DeviceUnknownIcon from '@mui/icons-material/DeviceUnknown';
-import {formatEnum} from "../utils";
+import {formatEnum, getIssueIcon} from "../utils";
 import {Link} from "react-router-dom";
 import {viewIssuePage} from "../const";
 
 const Issue = ({issue}) => {
-    const bugColorBySeverity = {
-        "TRIVIAL": "#00ac46",
-        "MINOR": "#fdc500",
-        "MAJOR": "#fd8c00",
-        "CRITICAL": "#dc0000",
-        "BLOCKER": "#780000"
-    };
-
-    const iconsByIssueType = {
-        "BUG": <BugReportIcon fontSize={"large"} htmlColor={bugColorBySeverity[issue.severity]}/>,
-        "DOCUMENTATION": <DescriptionIcon fontSize={"large"} htmlColor={"black"}/>,
-        "DUPLICATE": <ContentCopyIcon fontSize={"large"} htmlColor={"black"}/>,
-        "ENHANCEMENT": <AddBoxIcon fontSize={"large"} htmlColor={"black"}/>,
-        "HELP_WANTED": <HelpIcon fontSize={"large"} htmlColor={"black"}/>,
-        "INVALID": <ClearIcon fontSize={"large"} htmlColor={"black"}/>,
-        "QUESTION": <DeviceUnknownIcon fontSize={"large"} htmlColor={"black"}/>,
-        "WONT_FIX": <DoNotDisturbIcon fontSize={"large"} htmlColor={"black"}/>
-    };
-
     return (
         <Link to={viewIssuePage.replaceAll(":id", issue.id)} className={"issue-details"}>
-            <Box >
+            <Box>
                 <Box className={"issue-title-flex"}>
                     <Tooltip title={issue.type}>
-                        {iconsByIssueType[issue.type]}
+                        {getIssueIcon(issue)}
                     </Tooltip>
                     <p>{issue.title}</p>
                     <p>{formatEnum(issue.status)}</p>
