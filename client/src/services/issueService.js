ee034cca9e940d5e7614165d2eee3afbb1dd7b7c
Oskar Picus <oskarpicus@yahoo.com>
2021-12-13 18:06:13 +0200

    Implemented service for deleting issue

--- client/src/services/issueService.js
+++ client/src/services/issueService.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import {addIssueHttp, getAssignedIssuesHttp, getIssueByIdHttp, responseTypes} from "../components/const";
+import {
+    addIssueHttp,
+    deleteIssueByIdHttp,
+    getAssignedIssuesHttp,
+    getIssueByIdHttp,
+    responseTypes
+} from "../components/const";
 
 export const addIssue = async (token, issue) => {
     try {
@@ -48,6 +54,24 @@ export const getIssueById = async (token, id) => {
 
         const response = await axios.get(getIssueByIdHttp.URI.replaceAll(":id", id), config);
         response.data[responseTypes.keyFallback] = responseTypes.success;
+        return response.data;
+    } catch (error) {
+        error.response[responseTypes.key] = responseTypes.error;
+        return error.response;
+    }
+}
+
+export const deleteIssue = async (token, id) => {
+    try {
+        let config = {
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        };
+
+        const response = await axios.delete(deleteIssueByIdHttp.URI.replaceAll(":id", id), config);
+        response.data[responseTypes.key] = responseTypes.success;
         return response.data;
     } catch (error) {
         error.response[responseTypes.key] = responseTypes.error;
