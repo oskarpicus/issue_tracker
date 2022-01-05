e2d10780737dd590573ba0f5701d7c3af4c382ef
Oskar Picus <oskarpicus@yahoo.com>
2022-01-05 11:09:15 +0200

    Implemented endpoint for retrieving suggested severity

---
+++ artificial_intelligence/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+from artificial_intelligence.ai_models.SeverityClassifier import SeverityClassifier
+from artificial_intelligence.service.Service import Service
+
+app = Flask("Bugsby Artificial Intelligence API")
+service = Service(SeverityClassifier("resources/aiModels/severityModel.joblib"))
+
+
+@app.route("/suggested-severity")
+def suggested_severity():
+    title = request.args.get("title")
+    result = service.compute_suggested_severity(title)
+    return result.name
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
