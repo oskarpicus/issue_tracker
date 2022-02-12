98ab829cc45e596d73696a4f1ba47957c18ed853
Oskar Picus <oskarpicus@yahoo.com>
2022-02-12 09:38:45 +0200

    Refactored const.js

--- client/src/const.js
+++ client/src/const.js
@@ -1,209 +1,180 @@
-const websiteTitle = "Bugsby";
+export const websiteTitle = "Bugsby";
 
-const serverErrorMessage = "Server is not responding. Try again later";
+export const serverErrorMessage = "Server is not responding. Try again later";
 
-const createdAccountMessage = "Account created successfully";
+export const createdAccountMessage = "Account created successfully";
 
-// todo replace with just /users
+const apiUrl = "http://localhost:8080";
+
 /**
  * Object that encapsulated the details of the request made to the server to create a new account
  * @type {{method: string, URI: string}}
  */
-const createUserHttp = {method: "POST", URI: "http://localhost:8080/users"};
+export const createUserHttp = {method: "POST", URI: apiUrl + "/users"};
 
 /**
  * Object that encapsulates the details of the request made to the server to login
  * @type {{method: string, URI: string}}
  */
-const loginHttp = {method: "POST", URI: "http://localhost:8080/users/login"}
+export const loginHttp = {method: "POST", URI: apiUrl + "/users/login"};
 
 /**
  * Object that encapsulates the details of the request made to the server for accessing the projects of a user
  * @type {{method: string, URI: string}}
  */
-const getInvolvementsHttp = {method: "GET", URI: "http://localhost:8080/involvements?username=:username"}
+export const getInvolvementsHttp = {method: "GET", URI: apiUrl + "/involvements?username=:username"};
 
 /**
  * Object that encapsulates the details of the request made to the server for accessing a user by their username
  * @type {{method: string, URI: string}}
  */
-const getUserByUsernameHttp = {method: "GET", URI: "http://localhost:8080/users?username=:username"}
+export const getUserByUsernameHttp = {method: "GET", URI: apiUrl + "/users?username=:username"};
 
 /**
  * Object that encapsulates the details of the request made to the server for accessing a project by its identifier
  * @type {{method: string, URI: string}}
  */
-const getProjectByIdHttp = {method: "GET", URI: "http://localhost:8080/projects/:id"}
+export const getProjectByIdHttp = {method: "GET", URI: apiUrl + "/projects/:id"};
 
 /**
  * Object that encapsulates the details of the request made to the server for accessing all the available project roles
  * @type {{method: string, URI: string}}
  */
-const getRolesHttp = {method: "GET", URI: "http://localhost:8080/roles"}
+export const getRolesHttp = {method: "GET", URI: apiUrl + "/roles"};
 
 /**
  * Object that encapsulates the details of the request made to the server for adding a project
  * @type {{method: string, URI: string}}
  */
-const addProjectHttp = {method: "POST", URI: "http://localhost:8080/projects"};
+export const addProjectHttp = {method: "POST", URI: apiUrl + "/projects"};
 
 /**
  * Object that encapsulates the details of the request made to the server for retrieving all usernames
  * @type {{method: string, URI: string}}
  */
-const getAllUsernamesHttp = {method: "GET", URI: "http://localhost:8080/users/usernames"}
+export const getAllUsernamesHttp = {method: "GET", URI: apiUrl + "/users/usernames"};
 
 /**
  * Object that encapsulates the details of the request made to the server for adding a participant to a project
  * @type {{method: string, URI: string}}
  */
-const addParticipantHttp = {method: "POST", URI: "http://localhost:8080/involvements"};
+export const addParticipantHttp = {method: "POST", URI: apiUrl + "/involvements"};
 
 /**
  * Object that encapsulates the details of the request made to the server for retrieving the possible issue types
  * @type {{method: string, URI: string}}
  */
-const getIssueTypesHttp = {method: "GET", URI: "http://localhost:8080/issue-types"};
+export const getIssueTypesHttp = {method: "GET", URI: apiUrl + "/issue-types"};
 
 /**
  * Object that encapsulates the details of the request made to the server for retrieving the possible issue statuses
  * @type {{method: string, URI: string}}
  */
-const getStatusesHttp = {method: "GET", URI: "http://localhost:8080/statuses"};
+export const getStatusesHttp = {method: "GET", URI: apiUrl + "/statuses"};
 
 /**
  * Object that encapsulates the details of the request made to the server for retrieving the possible issue severities
  * @type {{method: string, URI: string}}
  */
-const getSeveritiesHttp = {method: "GET", URI: "http://localhost:8080/severities"};
+export const getSeveritiesHttp = {method: "GET", URI: apiUrl + "/severities"};
 
 /**
  * Object that encapsulates the details of the request made to the server for adding a new issue
  * @type {{method: string, URI: string}}
  */
-const addIssueHttp = {method: "POST", URI: "http://localhost:8080/issues"}
+export const addIssueHttp = {method: "POST", URI: apiUrl + "/issues"};
 
 /**
  * Object that encapsulates the details of the request made to the server for retrieving the assigned issues of a user
  * @type {{method: string, URI: string}}
  */
-const getAssignedIssuesHttp = {method: "GET", URI: "http://localhost:8080/issues?assignee=:username"}
+export const getAssignedIssuesHttp = {method: "GET", URI: apiUrl + "/issues?assignee=:username"};
 
 /**
  * Object that encapsulates the details of the request made to the server for retrieving an issue by its identifier
  * @type {{method: string, URI: string}}
  */
-const getIssueByIdHttp = {method: "GET", URI: "http://localhost:8080/issues/:id"}
+export const getIssueByIdHttp = {method: "GET", URI: apiUrl + "/issues/:id"};
 
 /**
  * Object that encapsulates the details of the request made to the server for deleting an issue by its identifier
  * @type {{method: string, URI: string}}
  */
-const deleteIssueByIdHttp = {method: "DELETE", URI: "http://localhost:8080/issues/:id"}
+export const deleteIssueByIdHttp = {method: "DELETE", URI: apiUrl + "/issues/:id"};
 
 /**
  * Object that encapsulates the details of the request made to the server for updating an issue
  * @type {{method: string, URI: string}}
  */
-const updateIssueHttp = {method: "PUT", URI: "http://localhost:8080/issues/:id"}
+export const updateIssueHttp = {method: "PUT", URI: apiUrl + "/issues/:id"};
 
-const getSuggestedSeverityHttp = {method: "GET", URI: "http://localhost:8080/suggested-severity?title=:title"}
+/**
+ * Object that encapsulates the details of the request made to the server for retrieving the suggested severity of an issue
+ * @type {{method: string, URI: string}}
+ */
+export const getSuggestedSeverityHttp = {method: "GET", URI: apiUrl + "/suggested-severity?title=:title"};
 
 /**
  * The page address for visiting the page dedicated for creating an account
  * @type {string}
  */
-const createAccountPage = "/create-account";
+export const createAccountPage = "/create-account";
 
 /**
  * The page address for logging in the application
  * @type {string}
  */
-const loginPage = "/";
+export const loginPage = "/";
 
 /**
  * The page address for accessing the error message page
  * @type {string}
  */
-const errorPage = "/error";
+export const errorPage = "/error";
 
 /**
  * The page address for visiting the projects of a specific user, based on their username
  * @type {string}
  */
-const viewProjectsPage = "/:username/projects";
+export const viewProjectsPage = "/:username/projects";
 
 /**
  * The page address for visiting the details page of a single project
  * @type {string}
  */
-const viewSingleProjectPage = "/projects/:id";
+export const viewSingleProjectPage = "/projects/:id";
 
 /**
  * The page address for visiting the form to add a project
  * @type {string}
  */
-const addProjectPage = "/add-project";
+export const addProjectPage = "/add-project";
 
 /**
  * The page address for visiting the form to add an issue
  * @type {string}
  */
-const addIssuePage = "/projects/:id/add-issue";
+export const addIssuePage = "/projects/:id/add-issue";
 
 /**
  * The page address for visiting the assigned issues of a user
  * @type {string}
  */
-const viewAssignedIssuesPage = "/:username/assigned-issues";
+export const viewAssignedIssuesPage = "/:username/assigned-issues";
 
 /**
  * The page address for visiting the page dedicated to a certain issue
  * @type {string}
  */
-const viewIssuePage = "/issues/:id";
+export const viewIssuePage = "/issues/:id";
 
 /**
  * Defines how a call to the backend can result. This information will be added in the "key" field of the response.
  * @type {{success: string, error: string, key: string}}
  */
-const responseTypes = {
+export const responseTypes = {
     key: "type",
     keyFallback: "responseKey",
     error: "error",
     success: "success"
-}
-
-export {
-    websiteTitle,
-    serverErrorMessage,
-    createdAccountMessage,
-    createUserHttp,
-    loginHttp,
-    getInvolvementsHttp,
-    getUserByUsernameHttp,
-    getProjectByIdHttp,
-    addParticipantHttp,
-    getIssueTypesHttp,
-    getStatusesHttp,
-    getSeveritiesHttp,
-    addIssueHttp,
-    getAssignedIssuesHttp,
-    getIssueByIdHttp,
-    deleteIssueByIdHttp,
-    updateIssueHttp,
-    getSuggestedSeverityHttp,
-    loginPage,
-    createAccountPage,
-    errorPage,
-    viewProjectsPage,
-    viewSingleProjectPage,
-    addProjectPage,
-    getRolesHttp,
-    addProjectHttp,
-    getAllUsernamesHttp,
-    addIssuePage,
-    viewAssignedIssuesPage,
-    viewIssuePage,
-    responseTypes
-}
+};
