2a15dd5e90557e8e0433368eaafcf0c27431de36
Oskar Picus <oskarpicus@yahoo.com>
2022-02-20 15:33:13 +0200

    Implemented server part of predicting issue type

--- artificial_intelligence/artificial_intelligence/service/Service.py
+++ artificial_intelligence/artificial_intelligence/service/Service.py
@@ -1,10 +1,13 @@
+from artificial_intelligence.ai_models.LabelClassifier import LabelClassifier
 from artificial_intelligence.ai_models.SeverityClassifier import SeverityClassifier
+from artificial_intelligence.model.IssueType import IssueType
 from artificial_intelligence.model.SeverityLevel import SeverityLevel
 
 
 class Service:
-    def __init__(self, severity_classifier: SeverityClassifier):
+    def __init__(self, severity_classifier: SeverityClassifier, label_classifier: LabelClassifier):
         self.__severity_classifier = severity_classifier
+        self.__label_classifier = label_classifier
 
     def compute_suggested_severity(self, title: str) -> SeverityLevel:
         """
@@ -13,3 +16,11 @@ class Service:
         :return: the predicted severity level of the issue with the corresponding title
         """
         return self.__severity_classifier.predict(title)
+
+    def computed_suggested_type(self, title: str) -> IssueType:
+        """
+        Method for predicting the issue type of an issue, based on its title, using a pre-trained AI model
+        :param title: the title of the issue to be classified
+        :return: the predicted issue type of the issue with the corresponding title
+        """
+        return self.__label_classifier.predict(title)
