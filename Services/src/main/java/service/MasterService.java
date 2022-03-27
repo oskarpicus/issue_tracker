18f2c240650a5d1134cba7971f35474d73555a04
Oskar Picus <oskarpicus@yahoo.com>
2022-03-27 20:45:35 +0300

    Fixed implementation of addParticipant

--- Services/src/main/java/service/MasterService.java
+++ Services/src/main/java/service/MasterService.java
@@ -104,7 +104,7 @@ public class MasterService implements Service {
         boolean isParticipant = userRequester.get()
                 .getInvolvements()
                 .stream()
-                .anyMatch(involvement1 -> involvement1.getProject().equals(involvement.getProject()));
+                .anyMatch(involvement1 -> involvement1.getProject().getId().equals(involvement.getProject().getId()));
         if (!isParticipant) {
             throw new UserNotInProjectException("Requester is not in the project");
         }
