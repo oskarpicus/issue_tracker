3ef6c0c95a35c17df7535f2f7a9330dc9eb22100
Oskar Picus <oskarpicus@yahoo.com>
2022-07-11 08:24:45 +0300

    Commented suggested type message

--- client/src/components/addIssueModal/AddIssueModal.js
+++ client/src/components/addIssueModal/AddIssueModal.js
@@ -24,16 +24,20 @@ const getSuggestedSeverityMessage = (suggestedSeverity) => {
 };
 
 const getSuggestedTypeMessage = (suggestedType) => {
-  return (
-    <span style={{display: "flex", marginBottom: "1vh", alignItems: "center"}}>
-        We think that this issue is a&nbsp;
-        <span className={"container-icon-text"}>
-            {getIssueIcon({"type": suggestedType})}
-            &nbsp;
-            {suggestedType}.
-        </span>
-    </span>
-  );
+    return (
+        <span/>
+    );
+    // TODO uncomment as a result of Task/#133
+  // return (
+  //   <span style={{display: "flex", marginBottom: "1vh", alignItems: "center"}}>
+  //       We think that this issue is a&nbsp;
+  //       <span className={"container-icon-text"}>
+  //           {getIssueIcon({"type": suggestedType})}
+  //           &nbsp;
+  //           {suggestedType}.
+  //       </span>
+  //   </span>
+  // );
 };
 
 const redirectToViewDuplicates = (issue, history) => {
